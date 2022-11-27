6b939eed014c1942d33d3174ebe617b5b651e457
= <=>
2022-11-27 18:36:27 +0100

    Added requirement #14

--- challenge.js
+++ challenge.js
@@ -83,153 +83,160 @@ async function showOptions() {
 
 // Una vez la ejecución termine, volveremos a mostrar el listado de requisitos para que el usuario pueda seleccionar otro
 
-do {
-  try {
-      options()
-      optionFromUser = await getUserOption();
+  do {
+    try {
+        options()
+        optionFromUser = await getUserOption();
 
-  } catch (error) {
-      console.error(error)
-      process.exit(0)
- }
-
- // 
- switch(optionFromUser) {
-      
-  case 1: 
-    // Se muestran todos los alumnos en formato tabla
-
-    console.table(students);
-    break;
-
-  case 2:
-    // Se muestra por consola la cantidad de alumnos
-
-    console.log(`La cantidad de alumnos es: ${students.length}`)
-    break;
-  
-  case 3:
-    // Se muestran todos los nombres de los alumnos por consola
-
-    for (let elements of students) {
-      console.log(`${elements.name}`)
-    }
-    break;
-
-  case 4:
-    // Se elimina el último alumno de la clase
-
-    let lastStudent = students.pop()
-    console.log(`Se ha eliminado el alumno: ${lastStudent.name}`)
-    console.log("Tabla de alumnos actualizada:")
-    console.table(students);
-    break;
-
-  case 5:
-    // Se elimina un alumno aleatoriamente
-
-    let randomStudent = students.splice(Math.floor(Math.random() * students.length),1);
-    randomStudent.forEach(function(element) {
-    console.log(`Se ha eliminado el alumno: ${element.name}`);
-    })
-    console.log("Tabla de alumnos actualizada:")
-    console.table(students);
-    break;
-
-  case 6:
-    // Se muestran por consola los datos de los alumnos con availableGenders 'female'
-
-    let femaleStudents = students.filter(student => student.gender === 'female');
-    console.log("Tabla de alumnos con género femenino:")
-    console.table(femaleStudents)
-    break;
-
-  case 7:
-    // Se muestra por consola el nº de chicas y chicos de la clase
-
-    let numberOfFemaleStudents = students.filter(student => student.gender === 'female');
-    let numberOfMaleStudents = students.filter(student => student.gender === 'male');
-    console.log(`La cantidad de chicas en la clase es: ${numberOfFemaleStudents.length}`)
-    console.log(`La cantidad de chicos en la clase es: ${numberOfMaleStudents.length}`)
-    break;
-
-  case 8:
-    // Se muestra true o false por consola si todos los alumnos son chicas
-
-    let allFemaleStudents = students.every(student => student.gender === 'female');
-    console.log(allFemaleStudents);
-    break;
-
-  case 9:
-    // Se muestran por consola los nombres de los alumnos entre 20 y 25 años
-
-    let ageOfStudents = students.filter(student => student.age >= 20 && student.age <= 25);
-    ageOfStudents.forEach(function(element) {
-    console.log(`Los alumnos que tienen entre 20 y 25 años son: ${element.name}`);
-    })
-    break;
-
-  case 10:
-    // Se añade un nuevo alumno con los datos facilitados
-  
-  case 11:
-    // Se muestra por consola el nombre del alumno más jóven
-
-    let minAgeStudent = Math.min(...students.map(student => student.age));
-    let minValues = students.filter(student => student.age == minAgeStudent);
-    minValues.forEach(function(element) {
-    console.log(`El alumno más jóven es: ${element.name}`);
-    })
-    break;
-
-case 12:
-    // Se muestra por consola la edad media de todos los alumnos
-
-    let avgAgeStudents = students.reduce((accum,a) => a.age + accum,0)/students.length
-    let intAvgStudents = parseInt(avgAgeStudents)
-    console.log(`La edad media de todos los alumnos es: ${intAvgStudents}`)
-    break;
-
-case 13:
-    // Se muestra por consola la edad media de los alumnos con género femenino
-
-    let ageOfFemaleStudents = students.filter(student => student.gender === 'female');
-    let avgAgeFemaleStudents = ageOfFemaleStudents.reduce((accum,a) => a.age + accum,0)/ageOfFemaleStudents.length
-    let intAvgFemaleStudents = parseInt(avgAgeFemaleStudents)
-    console.log(`La edad media de los alumnos con género femenino es: ${intAvgFemaleStudents}`)
-    break;
-
-case 14:
-    // Se añade una nota (0-10) de forma aleatroria a cada alumno
-    
-    function calculateRandomScore(min, max) {
-    const randomScore = Math.floor(Math.random() * (max - min + 1)) + min;
-    return randomNumber;
-    };
-
-    // Tengo que conseguir insertar el valor random obtenido como valor de la key score
-    
-case 15: 
-    // Se ordenan los alumnos alfabéticamente
-
-    students.sort(function (a, b) {
-      if (a.name < b.name) {
-        return -1;
-      }
-      if (a.name > b.name) {
-        return 1;
-      }
-      return 0;
-    });
-    
-    console.log("Tabla actualizada por orden alfabético: ")
-    console.table(students);
-
-    // Tarea pendiente: corregir uppercase/lowercase ya que el sort no los iguala
+    } catch (error) {
+        console.error(error)
+        process.exit(0);
   }
-  
-  // En caso de que el usuario pulse el 0 o un número no contemplado, la aplicación terminará
 
-} while (optionFromUser >= 1 && optionFromUser <= 18)
+    // 
+      switch(optionFromUser) {
+          
+        case 1: 
+          // Se muestran todos los alumnos en formato tabla
+
+          console.table(students);
+          break;
+
+        case 2:
+          // Se muestra por consola la cantidad de alumnos
+
+          console.log(`La cantidad de alumnos es: ${students.length}`)
+          break;
+        
+        case 3:
+          // Se muestran todos los nombres de los alumnos por consola
+
+          for (let elements of students) {
+            console.log(`${elements.name}`)
+          }
+          break;
+
+        case 4:
+          // Se elimina el último alumno de la clase
+
+          let lastStudent = students.pop()
+          console.log(`Se ha eliminado el alumno: ${lastStudent.name}`)
+          console.log("Tabla de alumnos actualizada:")
+          console.table(students);
+          break;
+
+        case 5:
+          // Se elimina un alumno aleatoriamente
+
+          let randomStudent = students.splice(Math.floor(Math.random() * students.length),1);
+          randomStudent.forEach(function(element) {
+          console.log(`Se ha eliminado el alumno: ${element.name}`);
+          })
+          console.log("Tabla de alumnos actualizada:")
+          console.table(students);
+          break;
+
+        case 6:
+          // Se muestran por consola los datos de los alumnos con availableGenders 'female'
+
+          let femaleStudents = students.filter(student => student.gender === 'female');
+          console.log("Tabla de alumnos con género femenino:")
+          console.table(femaleStudents)
+          break;
+
+        case 7:
+          // Se muestra por consola el nº de chicas y chicos de la clase
+
+          let numberOfFemaleStudents = students.filter(student => student.gender === 'female');
+          let numberOfMaleStudents = students.filter(student => student.gender === 'male');
+          console.log(`La cantidad de chicas en la clase es: ${numberOfFemaleStudents.length}`)
+          console.log(`La cantidad de chicos en la clase es: ${numberOfMaleStudents.length}`)
+          break;
+
+        case 8:
+          // Se muestra true o false por consola si todos los alumnos son chicas
+
+          let allFemaleStudents = students.every(student => student.gender === 'female');
+          console.log(allFemaleStudents);
+          break;
+
+        case 9:
+          // Se muestran por consola los nombres de los alumnos entre 20 y 25 años
+
+          let ageOfStudents = students.filter(student => student.age >= 20 && student.age <= 25);
+          ageOfStudents.forEach(function(element) {
+          console.log(`Los alumnos que tienen entre 20 y 25 años son: ${element.name}`);
+          })
+          break;
+
+        case 10:
+          // Se añade un nuevo alumno con los datos facilitados
+        
+        case 11:
+          // Se muestra por consola el nombre del alumno más jóven
+
+          let minAgeStudent = Math.min(...students.map(student => student.age));
+          let minValues = students.filter(student => student.age == minAgeStudent);
+          minValues.forEach(function(element) {
+          console.log(`El alumno más jóven es: ${element.name}`);
+          })
+          break;
+
+      case 12:
+          // Se muestra por consola la edad media de todos los alumnos
+
+          let avgAgeStudents = students.reduce((accum,a) => a.age + accum,0)/students.length
+          let intAvgStudents = parseInt(avgAgeStudents)
+          console.log(`La edad media de todos los alumnos es: ${intAvgStudents}`)
+          break;
+
+      case 13:
+          // Se muestra por consola la edad media de los alumnos con género femenino
+
+          let ageOfFemaleStudents = students.filter(student => student.gender === 'female');
+          let avgAgeFemaleStudents = ageOfFemaleStudents.reduce((accum,a) => a.age + accum,0)/ageOfFemaleStudents.length
+          let intAvgFemaleStudents = parseInt(avgAgeFemaleStudents)
+          console.log(`La edad media de los alumnos con género femenino es: ${intAvgFemaleStudents}`)
+          break;
+
+      case 14:
+          // Se añade una nota (0-10) de forma aleatroria a cada alumno
+
+          function randomNumberForScore(min, max) {
+          return Math.floor(Math.random() * (max - min + 1) + min)
+          }
+
+          for (let student in students) {
+            let newRandomScore = randomNumberForScore(0, 10) // se establece el parámetro entre 0 y 10
+            students[student]["examScores"].push(newRandomScore);
+          };
+          console.table(students)
+          break;
+
+          // Tengo que conseguir insertar el valor random obtenido como valor de la key score
+          
+      case 15: 
+          // Se ordenan los alumnos alfabéticamente
+
+          students.sort(function (a, b) {
+            if (a.name < b.name) {
+              return -1;
+            }
+            if (a.name > b.name) {
+              return 1;
+            }
+            return 0;
+          });
+          
+          console.log("Tabla actualizada por orden alfabético: ")
+          console.table(students);
+
+          // Tarea pendiente: corregir uppercase/lowercase ya que el sort no los iguala
+      }
+
+    // En caso de que el usuario pulse el 0 o un número no contemplado, la aplicación terminará
+
+  } while (optionFromUser >= 1 && optionFromUser <= 15)
+
 }
-showOptions();
+    showOptions();
